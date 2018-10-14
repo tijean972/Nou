5762ebbff039dc8f06bf27e952c0aadb76cb847e
Arnaud Miredin <arnaudmiredin@MacBook-Air-de-Arnaud.local>
2018-10-15 00:01:15 +0200

    Mise en place vue mes annonces

--- src/pages/mes-annonces/mes-annonces.ts
+++ src/pages/mes-annonces/mes-annonces.ts
@@ -30,23 +30,25 @@ export class MesAnnoncesPage {
 
   //private Annonces: AngularFireList<any>;
   private user: any;
-  
+  public Annonces: annonce[] = [];
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private afDatabase: AngularFireDatabase) {
     this.user = this.navParams.get('user');
     const listAnnonce = this.afDatabase.list('/Annonce', ref => ref.orderByChild('idEmmetteur').equalTo(this.user.uid));
-    const Annonces = listAnnonce.valueChanges();
-    Annonces.subscribe(console.log);
-    //this.afDatabase.list<annonce>('/Annonce').valueChanges().subscribe(console.log);
-    //console.log("c'est le user:" + JSON.stringify(this.user));
-    console.log("Tableau annonces:" + JSON.stringify(Annonces));
+    listAnnonce.valueChanges().subscribe((Annon:annonce[]) => {
+      Annon.forEach((ann) => {
+        //console.log(ann.Message);
+        this.Annonces.push(ann);
+      })
+    });
   }
 
   ngOnInit(){
   }
 
   addAnonce(){
-    this.navCtrl.setRoot(AddAnnoncesPage, {user:this.user});
+    this.navCtrl.push(AddAnnoncesPage, {user:this.user});
   }
 
 
