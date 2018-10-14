836507fe06f6c86092a5c1783c51b82d779cb624
Arnaud Miredin <arnaudmiredin@MacBook-Air-de-Arnaud.local>
2018-10-14 10:35:44 +0200

    Ionic pro installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,13 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-//DAO
+//IONIC PRO
+import { Pro } from '@ionic/pro';
+import { Injectable, Injector } from '@angular/core';
+
+Pro.init('2e38c784', {
+  appVersion: '1'
+})
 
 
 
@@ -49,10 +55,34 @@ const Enrironnement = {
   }
 };
 
-
+// Déclaration module multi langue
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
 };
+
+// Ionic pro
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
+
+
 
 
 @NgModule({
